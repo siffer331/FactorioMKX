af644860cb02cc85e94ca98cee89654121fc6346
Tyfl0n <71282715+Tyfl0n@users.noreply.github.com>
2020-10-31 23:27:07 +0100

    stuff

--- prototypes/recipe/fish-mk2.lua
+++ prototypes/recipe/fish-mk2.lua
@@ -4,12 +4,18 @@ data:extend(
     type = "recipe",
     name = "fish-mk2",
     enabled = true,
-    energy_required = 86400,
+    energy_required = 90,
     category = "crafting-with-fish",
     ingredients =
     {
-      {"speed-module", 4},
-      {"assembling-machine-2", 2}
+      {"uranium-235", 500},
+      {"raw-fish", 1},
+      {"arithmetic-combinator", 100},
+      {"decider-combinator", 100},
+      {"constant-combinator", 10},
+      {"processing-unit", 100},
+      {"fusion-reactor-equipment-mk2", 1},
+      {"battery-mk2-equipment", 1},
     },
     result = "fish-mk2"
   },
